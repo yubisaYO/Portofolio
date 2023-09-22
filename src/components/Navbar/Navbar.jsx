f0a36e2db708a3abbedc569a25c31fffe20eb6cf
unknown <charles.liu595@gmail.com>
2023-09-22 13:40:18 +0700

    second commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,66 +1,84 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const Navbar = () => {
   const [menu, setMenu] = useState(false);
+  const menuRef = useRef(null);
+
+  // Fungsi untuk menampilkan atau menyembunyikan menu dengan efek fade
+  const toggleMenu = () => {
+    if (!menu) {
+      // Jika menu belum terbuka, tampilkan menu dengan efek fade in
+      setMenu(true);
+    } else {
+      // Jika menu sudah terbuka, beri efek fade out
+      menuRef.current.classList.add("animate__fadeOutUp");
+      setTimeout(() => {
+        setMenu(false);
+        menuRef.current.classList.remove("animate__fadeOutUp");
+      }, 300); // Ubah angka 300 sesuai dengan durasi animasi CSS (ms)
+    }
+  };
 
   return (
     <div
       className={`fixed top-0 w-full  bg-white py-6 flex justify-around items-center shadow-md mx-auto transition duration-150 ease-out z-50`}
     >
-      <h1 className="font-bold text-xl">PORTOFOLIO.</h1>
+      <h1 className="font-bold text-xl">PORTFOLIO.</h1>
 
       <ul className="hidden md:flex text-sm items-center uppercase font-semibold">
-        <li className="mr-3">
+        {/* Tambahkan onClick untuk menutup menu saat item menu diklik */}
+        <li className="mr-3" onClick={toggleMenu}>
           <a href="#" className="hover:opacity-80">
             Home
           </a>
         </li>
-        <li className="mr-3">
+        <li className="mr-3" onClick={toggleMenu}>
           <a href="#about" className="hover:opacity-80">
             About
           </a>
         </li>
-        <li className="mr-3">
+        <li className="mr-3" onClick={toggleMenu}>
           <a href="#work" className="hover:opacity-80">
             Work
           </a>
         </li>
-        <li className="">
+        <li className="" onClick={toggleMenu}>
           <a href="#contact" className="hover:opacity-80">
             Contact
           </a>
         </li>
       </ul>
-      {/* <span className="hidden md:flex items-center text-2xl cursor-pointer">
-        <i className="bx bx-moon"></i>
-      </span> */}
       <span className="flex items-center md:hidden text-2xl cursor-pointer z-10">
-        <i className="bx bx-menu" onClick={() => setMenu(!menu)}></i>
+        <i className="bx bx-menu" onClick={toggleMenu}></i>
       </span>
-      {menu && (
-        <ul className="absolute md:hidden text-sm items-center uppercase font-medium w-full bg-white transition duration-300 ease-in-out top-14 text-center shadow-md border-2 border-t-gray-200">
-          <li className="p-4">
-            <a href="#" className="hover:opacity-80">
-              Home
-            </a>
-          </li>
-          <li className="p-4">
-            <a href="#about" className="hover:opacity-80">
-              About
-            </a>
-          </li>
-          <li className="p-4">
-            <a href="#work" className="hover:opacity-80">
-              Work
-            </a>
-          </li>
-          <li className=" p-4">
-            <a href="#contact" className="hover:opacity-80">
-              Contact
-            </a>
-          </li>
-        </ul>
-      )}
+      {/* Tambahkan kelas 'menu-open' saat menu terbuka */}
+      <ul
+        ref={menuRef}
+        className={`absolute md:hidden text-sm items-center uppercase font-semibold w-full bg-white top-14 text-center shadow-md border-2 border-t-gray-200 ${
+          menu ? "menu-open animate__animated animate__fadeInDown" : "hidden"
+        } space-y-6 py-4 rounded-b-lg`}
+      >
+        <li className="p-4">
+          <a href="#" className="hover:opacity-80">
+            Home
+          </a>
+        </li>
+        <li className="p-4">
+          <a href="#about" className="hover:opacity-80">
+            About
+          </a>
+        </li>
+        <li className="p-4">
+          <a href="#work" className="hover:opacity-80">
+            Work
+          </a>
+        </li>
+        <li className=" p-4">
+          <a href="#contact" className="hover:opacity-80">
+            Contact
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
